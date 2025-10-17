a6a290ddf996309cf6789b284e5e992cdde44969
d3val <devalt5008@gmail.com>
2025-10-16 23:09:33 -0600

    Player walking animation added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,11 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     public float speed = 10.0f;
     Vector2 playerInput;
+    [Header("Player inputs")]
     [SerializeField] InputActionAsset actionAsset;
     private InputAction moveAction;
+    [Header("Player animations")]
+    [SerializeField] Animator animator;
 
     private void Awake()
     {
@@ -32,6 +35,11 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        UpdateRenderer();
+    }
+
     private void FixedUpdate()
     {
         Move();
@@ -41,6 +49,22 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 playerDirection = new Vector3(playerInput.x, 0, playerInput.y);
         rb.AddForce(playerDirection * speed, ForceMode.Force);
+        animator.SetFloat("speed", rb.linearVelocity.magnitude);
+    }
+
+    private void UpdateRenderer()
+    {
+        float currentXScale = transform.localScale.x;
+        if (playerInput.x < 0)
+        {
+            currentXScale = Mathf.Abs(currentXScale);
+        }
+        else if (playerInput.x > 0)
+        {
+            currentXScale = -Mathf.Abs(currentXScale);
+        }
+
+        transform.localScale = new Vector3(currentXScale, transform.localScale.y, transform.localScale.z);
     }
 
     private void ReadMove(InputAction.CallbackContext ctx)
