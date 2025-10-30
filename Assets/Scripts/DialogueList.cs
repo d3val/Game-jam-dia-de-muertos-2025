bf1b770157af46213fbdeec4aa3d31831f35f4f8
Eduardo Alvarez <90423330+d3val@users.noreply.github.com>
2025-10-29 20:34:08 -0600

    Dialogues (#7)
    * Prefabs update
    
    * UI interaction indicator
    
    * Items UI added, onDialoguesEnd event added

--- Assets/Scripts/DialogueList.cs
+++ Assets/Scripts/DialogueList.cs
@@ -1,20 +1,29 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogueList : MonoBehaviour
 {
     [SerializeField] private List<Dialogue> dialogues;
+    [SerializeField] private UnityEvent OnDialoguesEnd;
     private int currentDialogue = 0;
+
+    private void Start()
+    {
+        Debug.Log("Total de dialogos: " + dialogues.Count);
+    }
 
     public void NextDialogue()
     {
         if (currentDialogue >= dialogues.Count)
         {
+            OnDialoguesEnd?.Invoke();
             EndConversation();
             return;
         }
 
-        DialogueUI.Instance.SetDialogue(dialogues[currentDialogue].dialogueContent);
+        DialogueUI.Instance.SetDialogue(dialogues[currentDialogue].speakerName, dialogues[currentDialogue].dialogueContent);
+        dialogues[currentDialogue].OnDialogueStart?.Invoke();
         currentDialogue++;
     }
 
